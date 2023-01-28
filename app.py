a0b24eb0be8644b39d19fad8fc3499c0c0db01bc
akatrosh <amarczak@edu.cdv.pl>
2023-01-28 18:16:10 +0100

    login

--- app.py
+++ app.py
@@ -3,9 +3,58 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 class BoardGame(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,19 +65,15 @@ class BoardGame(db.Model):
 
 with app.app_context():
     db.create_all()
-
-
-
-@app.route('/edit_game', methods=['POST'])
-def edit_game():
-    game_id = request.form['game_id']
-    game = BoardGame.query.get(game_id)
-    game.name = request.form['name']
-    game.players = request.form['players']
+    new_user = User(username='admin', password='admin')
+    db.session.add(new_user)
     db.session.commit()
-    return redirect(url_for('index'))
+
+
+
 
 @app.route('/rate_game', methods=['POST'])
+@login_required
 def rate_game():
     game_id = request.form['game_id']
     game = BoardGame.query.get(game_id)
@@ -58,7 +103,10 @@ def index():
 
 
     return render_template('index.html', games=games)
+
+
 @app.route('/delete_game', methods=['POST'])
+@login_required
 def delete_game():
     game_id = request.form['game_id']
     game = BoardGame.query.get(game_id)
@@ -67,4 +115,5 @@ def delete_game():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    app.secret_key = 'ultra_secret_key'
     app.run(debug=False, port=5000, host='0.0.0.0')
